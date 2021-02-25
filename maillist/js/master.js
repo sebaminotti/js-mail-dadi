d30f648f9db68e548e2c57088f7052c97baf6c9e
sebastiano minotti <seba.minotti@gmail.com>
2021-02-25 17:40:03 +0100

    correzione errore nel codice js di maillist

--- maillist/js/master.js
+++ maillist/js/master.js
@@ -1,19 +1,16 @@
+  
 var mailList = ["sebaminuz@gmail.com","oliver@gmail.com","madre@gmail.com","allison@gmail.com","gian@gmail.com"];
 console.log(mailList);
 var mail = prompt("Inserire la mail d'accesso!");
 console.log(mail);
-
-for (var i  = 0 ; i < mailList.length; i++ ) {
-
-}
-
-if (mailList[i]  == mail) {
-    alert("sei in lista ");
+for(var i = 0 ; i < mailList.length; i++ ) {
+if (mailList[i]  == mail ) {
     document.getElementById("mail").innerHTML =("sei in lista");
-    }
-else if (mailList[i] != mail) {
-    alert ("non sei ancora registrato");
-    document.getElementById("mail").innerHTML = ("non sei in lista");
-    }
+    break;
+    } 
 
-
+else   {
+   
+    document.getElementById("mail").innerHTML = ("non sei in lista");}
+    
+}
